140e5ee6c9b2fae5fa2ab3a8f5a7c4c04fd87dc0
tineetinytin <christine.elene@gmail.com>
2021-02-19 18:29:48 -0800

    final changes

---
+++ scripts.js
@@ -0,0 +1,126 @@
+
+
+//NUMBER 1
+
+
+// let num1 = window.prompt('Input the First integer');
+// let num2 = window.prompt('Input the second integer');
+                                                 
+// if(parseInt(num1) > parseInt(num2)) 
+//   { 
+//      document.write(`The larger of ${num1} and ${num2} is ${num1}`);
+//   }   
+// else
+//   if(parseInt(num2) > parseInt(num1)) 
+//   {
+//   document.write(`The larger of ${num1} and ${num2} is ${num2}`);
+//   }                  
+// else
+//   {
+//   document.write(`The values of ${num1} and ${num2} are equal`);
+//   }
+
+
+//NUMBER 2
+
+
+// let lang = prompt('How do you want me to translate "Hello World" (es / de / en / fr) ?');
+
+// if( lang === 'es') {
+//     document.write('Hello World translated in Spanish is: Hola Mundo.');
+// } else if(lang === 'de') {
+//     document.write('Hello World translated in German is: Hallo Welt.');
+// } else if(lang === 'en') {
+//     document.write('Hello World translated in English is: Hello World.');
+// } else if(lang === 'fr') {
+//     document.write('Hello World translated in French is: Bonjour le monde.');
+// } else {
+//     document.write('Hello World translated in English is: Hello World.');
+// }
+
+
+//NUMBER 3
+
+
+// var coinFlip = Math.round(Math.random());
+
+// var choice = prompt('Heads or Tails');
+
+// if (coinFlip === 0) {
+//     var flipResult = 'heads';
+// } else {
+//     var flipResult = 'tails';
+// }
+
+// if (flipResult === choice) {
+//     if (flipResult === 'heads') {
+//         alert('The flip was heads and you chose heads...you win!');
+//     } else {
+//         alert('The flip was tails and you chose tails...you win!');
+//     }
+// } else {
+//     if (flipResult === 'heads') {
+//         alert('The flip was heads and you chose tails...you lose!');
+//     } else {
+//         alert('The flip was tails and you chose heads...you lose!');
+//     }
+// }
+
+
+//NUMBER 4
+
+
+// let score = prompt('Give me a number between 1 and 100.')
+
+// if (score >= 101) {
+//     alert('We only accept numbers between 1 and 100. Please try again.');
+// } else if(score >= 90) {
+//     console.log('You received an A');
+// } else if(score >= 80) {
+//     console.log('You received a B');
+// } else if(score >= 70) {
+//     console.log('You received a C');
+// } else if(score >= 60) {
+//     console.log('You received a D');
+// } else {
+//     console.log('You received an F');
+// }
+
+
+//NUMBER 5
+
+
+// let user = prompt('Are you ready to play?')
+
+// if (user === 'yes') {
+//     alert('You are in a dark, dingy, and humid cave searching for the lost treasure of Captain Chingadera. You are disoriented, lost, hungry and extremely thirsty. You see a speck of light in the distance ahead of you, something shimmering to your right, and the sound of running water to your left. Your back is against the wall.');
+
+//     let user2 = prompt('Which direction would you like to head (please enter forward, left, or right).');
+//     if (user2 === 'forward') {
+//         alert('You walk 100 yards and safely make your way out of the cave.');
+//     } else if (user2 === 'left') {
+//         alert('Your thirst has gotten the better of you. You trip on a rock, hit your head, and fall into a pool of water and drown!');
+//     } else if (user2 === 'right') {
+//         alert('You found the gold! You walk into a small room and see thousands of gold coins, jewels, chalices, and more. You jump into the pile of gold in celebration and immediately a hidden door slams down and traps you in the room forever!');
+//     } else {
+//         alert('The ghost of Captain Chingadera has condemned you to eternal damnation and you shall now burn in the hot excoriation for lifeless lowlifes for not choosing the correct option.');
+//     }
+
+//     alert('GAME OVER!');
+
+//     let user3 = prompt('Please rate the game between 1 and 10');
+
+//     if (user3 >= 6) {
+//         document.write('Thank you and play again.');
+//     } else if (user3 >= 1) {
+//         document.write('Thank you and we are working hard to improve the game.');
+//     } else {
+//         document.write('Thank you for playing!');
+//     }
+
+// } else {
+//     document.write('Thank you and play again sometime!');
+// }
+
+
+
